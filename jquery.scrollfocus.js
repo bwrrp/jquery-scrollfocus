82eb82de3a68835ec51e6fdbfaa8cdfed94f905a
Stef Busking <stef.busking@gmail.com>
2013-03-22 16:00:39 +0100

    Dealing with the window across browsers.

--- jquery.scrollfocus.js
+++ jquery.scrollfocus.js
@@ -48,27 +48,36 @@
 
 	function ScrollBox(element) {
 		Box.call(this, {
-			top: -element.scrollTop,
-			bottom: -element.scrollTop + element.scrollHeight,
-			left: -element.scrollLeft,
-			right: -element.scrollLeft + element.scrollWidth
+			top: 0,
+			bottom: 0,
+			left: 0,
+			right: 0
 		}, new OffsetBox(element));
 
 		this.element = element;
+		this.refresh();
 	}
 	ScrollBox.prototype = new Box();
 	ScrollBox.prototype.constructor = ScrollBox;
 
-	ScrollBox.prototype.scrollTo = function(left, top) {
-		console.log('scrolling', this.element, 'from', this.left, this.top, 'to', left, top);
-		this.element.scrollTop = top;
+	ScrollBox.prototype.refresh = function() {
 		this.top = -this.element.scrollTop;
-		this.element.scrollLeft = left;
+		this.bottom = -this.element.scrollTop + this.element.scrollHeight;
 		this.left = -this.element.scrollLeft;
+		this.right = -this.element.scrollLeft + this.element.scrollWidth;
+	};
+
+	ScrollBox.prototype.scrollTo = function(left, top) {
+		console.log('scrolling', this.element, 'from', -this.left, -this.top, 'to', left, top);
+		this.element.scrollTop = top;
+		this.element.scrollLeft = left;
+		this.refresh();
 	};
 
 	function OffsetBox(element) {
-		var offsetParentBox = element.offsetParent ? new ScrollBox(element.offsetParent) : null;
+		var offsetParentBox = element.offsetParent && element.offsetParent !== element.ownerDocument.body ?
+			new ScrollBox(element.offsetParent) :
+			new WindowBox(element.ownerDocument);
 
 		Box.call(this, {
 			top: element.offsetTop,
@@ -82,6 +91,36 @@
 	OffsetBox.prototype = new Box();
 	OffsetBox.prototype.constructor = OffsetBox;
 
+	function WindowBox(document) {
+		Box.call(this, {
+			top: 0,
+			bottom: 0,
+			left: 0,
+			right: 0
+		});
+
+		this.document = document;
+		this.refresh();
+	}
+	WindowBox.prototype = new Box();
+	WindowBox.prototype.constructor = WindowBox;
+
+	WindowBox.prototype.refresh = function() {
+		var window = this.document.defaultView,
+			documentElement = this.document.documentElement;
+		this.top = -window.pageYOffset;
+		this.bottom = -window.pageYOffset + documentElement.offsetHeight;
+		this.left = -window.pageXOffset;
+		this.right = -window.pageXOffset + documentElement.offsetWidth;
+	};
+
+	WindowBox.prototype.scrollTo = function(left, top) {
+		var window = this.document.defaultView;
+		console.log('scrolling', window, 'from', -this.left, -this.top, 'to', left, top);
+		window.scrollTo(left, top);
+		this.refresh();
+	};
+
 	function getRangeBox(range) {
 		var bounds = range.getBoundingClientRect(),
 			container = range.commonAncestorContainer;
@@ -91,12 +130,12 @@
 	}
 
 	function scrollerize(target, box, until) {
-		if (box.parentBox) {
-			if (box.parentBox.scrollTo) {
-				var relativeTarget = target.relativeTo(box.parentBox);
-				box.parentBox.scrollTo(relativeTarget.left, relativeTarget.top);
+		if (box) {
+			if (box.scrollTo) {
+				var relativeTarget = target.relativeTo(box);
+				box.scrollTo(relativeTarget.left, relativeTarget.top);
 
-				if (box.parentBox.element === until || $.contains(box.parentBox.element, until))
+				if (box.element && (box.element === until || $.contains(box.element, until)))
 					return;
 			}
 			scrollerize(target, box.parentBox, until);
