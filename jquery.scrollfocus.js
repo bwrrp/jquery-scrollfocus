487bdc9877dced0f2346929ff3361ff4b2aa81f2
Stef Busking <stef.busking@gmail.com>
2013-10-25 12:14:09 +0200

    Rewrite and simplify based on jQuery.fn.offset to avoid incorrect offsetTop values reported by browsers.

--- jquery.scrollfocus.js
+++ jquery.scrollfocus.js
@@ -1,123 +1,4 @@
-(function($) {
-
-	function Point(left, top, offsetParent) {
-		if (!arguments.length)
-			return;
-
-		this.left = left;
-		this.top  = top;
-
-		this.offsetParent = offsetParent;
-	}
-
-	Point.prototype.absolute = function() {
-		if (!this.offsetParent)
-			return this;
-
-		var origin = this.offsetParent.absolute();
-
-		return new Point(
-			origin.left + this.left,
-			origin.top  + this.top,
-			null);
-	};
-
-	Point.prototype.relativeTo = function(point) {
-		var newOrigin = point ? point.absolute() : {left: 0, top: 0},
-			self = this.absolute();
-		return new Point(
-			self.left - newOrigin.left,
-			self.top  - newOrigin.top,
-			point);
-	};
-
-	function Box(left, top, width, height, parentBox) {
-		if (!arguments.length)
-			return;
-
-		this.origin = new Point(left, top, parentBox && parentBox.origin);
-
-		this.width  = width;
-		this.height = height;
-
-		this.parentBox = parentBox;
-	}
-
-	Box.prototype.topLeft = function() {
-		return this.origin;
-	};
-
-	Box.prototype.bottomRight = function() {
-		return new Point(this.width, this.height, this.origin);
-	};
-
-	Box.prototype.relativeTo = function(box) {
-		var newOrigin = this.origin.relativeTo(box && box.origin);
-		return new Box(newOrigin.left, newOrigin.top, this.width, this.height, box);
-	};
-
-	function ScrollBox(element) {
-		Box.call(this, 0, 0, 0, 0, new OffsetBox(element));
-
-		this.element = element;
-		this.refresh();
-	}
-	ScrollBox.prototype = new Box();
-	ScrollBox.prototype.constructor = ScrollBox;
-
-	ScrollBox.prototype.refresh = function() {
-		this.origin.left = -this.element.scrollLeft;
-		this.origin.top  = -this.element.scrollTop;
-		this.width  = this.element.scrollWidth;
-		this.height = this.element.scrollHeight;
-	};
-
-	ScrollBox.prototype.scrollTo = function(left, top) {
-		this.element.scrollTop = top;
-		this.element.scrollLeft = left;
-		this.refresh();
-	};
-
-	function OffsetBox(element) {
-		var offsetParentBox = element.offsetParent && element.offsetParent !== element.ownerDocument.body ?
-			new ScrollBox(element.offsetParent) :
-			new WindowBox(element.ownerDocument);
-
-		Box.call(this,
-			element.offsetLeft,
-			element.offsetTop,
-			element.offsetWidth,
-			element.offsetHeight,
-			offsetParentBox);
-
-		this.element = element;
-	}
-	OffsetBox.prototype = new Box();
-	OffsetBox.prototype.constructor = OffsetBox;
-
-	function WindowBox(document) {
-		Box.call(this, 0, 0, 0, 0, null);
-
-		this.document = document;
-		this.refresh();
-	}
-	WindowBox.prototype = new Box();
-	WindowBox.prototype.constructor = WindowBox;
-
-	WindowBox.prototype.refresh = function() {
-		var window = this.document.defaultView,
-			documentElement = this.document.documentElement;
-		this.origin.left = -window.pageXOffset;
-		this.origin.top  = -window.pageYOffset;
-		this.width  = documentElement.offsetWidth;
-		this.height = documentElement.offsetHeight;
-	};
-
-	WindowBox.prototype.scrollTo = function(left, top) {
-		var window = this.document.defaultView;
-		window.scrollTo(left, top);
-		this.refresh();
-	};
+(function($, undefined) {
 
 	function isInvalidRect(bounds) {
 		return bounds.left === 0 && bounds.top === 0 && bounds.right === 0 && bounds.bottom === 0;
@@ -170,34 +51,6 @@
 		return bounds;
 	}
 
-	function getRangeBox(range) {
-		var bounds = getRangeBounds(range),
-			container = range.commonAncestorContainer;
-		if (container.nodeType !== 1)
-			container = container.parentNode;
-		return new Box(
-				bounds.left,
-				bounds.top,
-				bounds.right - bounds.left,
-				bounds.bottom - bounds.top,
-				null)
-			.relativeTo(new ScrollBox(container));
-	}
-
-	function getOffsetFromBox(point, box) {
-		var offset = point.relativeTo(box.origin);
-		if (offset.left >= 0 && offset.left < box.width)
-			offset.left = 0;
-		if (offset.top >= 0 && offset.top < box.height)
-			offset.top = 0;
-		if (offset.left >= box.width)
-			offset.left -= box.width;
-		if (offset.top >= box.height)
-			offset.top -= box.height;
-
-		return offset;
-	}
-
 	function isRange(obj) {
 		return !!obj.startContainer;
 	}
@@ -206,127 +59,117 @@
 		point: 'left top'
 	};
 
-	function adjustViewport(viewportBox, viewportOptions) {
-		var padding = viewportOptions.padding || 0,
-			width = viewportOptions.width || (viewportBox ? viewportBox.width : scrollable.width),
-			height = viewportOptions.height || (viewportBox ? viewportBox.height : scrollable.height);
-		return new Box(
-			(viewportOptions.left + padding) || 0,
-			(viewportOptions.top  + padding) || 0,
-			width - 2 * padding,
-			height - 2 * padding,
-			viewportBox);
-	}
-
-	function shrinkViewport(viewportBox, toPoint) {
-		// Adjust viewport box to cover only the requested point
-		var adjustedBox = new Box(
-			0, 0,
-			viewportBox.width,
-			viewportBox.height,
-			viewportBox);
-		var toPointParts = toPoint.split(/\s+/);
-		while (toPointParts.length) {
-			var toPointPart = toPointParts.pop();
-			switch (toPointPart.toLowerCase()) {
-				case 'left':
-					adjustedBox.origin.left = 0;
-					adjustedBox.width = 0;
-					break;
-				case 'top':
-					adjustedBox.origin.top = 0;
-					adjustedBox.height = 0;
-					break;
-				case 'right':
-					adjustedBox.origin.left = viewportBox.width;
-					adjustedBox.width = 0;
-					break;
-				case 'bottom':
-					adjustedBox.origin.top = viewportBox.height;
-					adjustedBox.height = 0;
-					break;
+	function completeBox(box, element) {
+		if (box.width === undefined) {
+			if (box.right === undefined) {
+				box.width = element.width();
+			} else {
+				box.width = box.right - box.left;
 			}
 		}
-		return adjustedBox;
-	}
+		if (box.right === undefined) {
+			box.right = box.left + box.width;
+		}
 
-	function getTargetPoint(targetBox, point) {
-		// Determine target point
-		var targetPoint = new Point(0, 0, targetBox.origin);
-		if (typeof point === 'string') {
-			var pointParts = point.split(/\s+/);
-			while (pointParts.length) {
-				var pointPart = pointParts.pop();
-				switch (pointPart.toLowerCase()) {
-					case 'left':
-						targetPoint.left = 0;
-						break;
-					case 'top':
-						targetPoint.top = 0;
-						break;
-					case 'right':
-						targetPoint.left = targetBox.width;
-						break;
-					case 'bottom':
-						targetPoint.top = targetBox.height;
-						break;
-					case 'center':
-						targetPoint.left = targetBox.width / 2;
-						break;
-					case 'middle':
-						targetPoint.top = targetBox.height / 2;
-						break;
-				}
+		if (box.height === undefined) {
+			if (box.bottom === undefined) {
+				box.height = element.height();
+			} else {
+				box.height = box.bottom - box.top;
 			}
 		}
-		return targetPoint;
+		if (box.bottom === undefined) {
+			box.bottom = box.top + box.height;
+		}
 	}
 
-	function isWindow(obj) {
-		return !!obj.document;
+	function insetBox(box, padding) {
+		box.top += padding;
+		box.left += padding;
+		box.bottom -= padding;
+		box.right -= padding;
+		box.width -= 2 * padding;
+		box.height -= 2 * padding;
+	}
+
+	function computeScrollOffset(targetBox, viewportBox, options) {
+		// TODO: determine amount to scroll based on options
+		return {
+			left: targetBox.left - viewportBox.left,
+			top: targetBox.top - viewportBox.top
+		};
 	}
 
 	$.fn.scrollFocus = function(options) {
-		$(this).each(function() {
-			var target = options.target;
-			if (isRange(options) || isElement(options)) {
-				target = options;
-				options = {};
+		var target = options && options.target;
+		if (!target) {
+			target = options;
+			options = {};
+		}
+
+		if (!target || (target.jquery && !target.length))
+			return;
+
+		// Apply defaults
+		options = $.extend({}, defaultOptions, options);
+
+		// Scroll elements
+		$(this.get().reverse()).each(function() {
+			var scrollable = $(this),
+				document = this.ownerDocument || this,
+				window = $.isWindow(this) ? this : document.defaultView,
+				pageXOffset = window.pageXOffset,
+				pageYOffset = window.pageYOffset;
+
+			// Determine target relative to document
+			var targetBox;
+			if (isRange(target)) {
+				var viewportRelativeBounds = getRangeBounds(target);
+				targetBox = {
+					left:   viewportRelativeBounds.left   + pageXOffset,
+					right:  viewportRelativeBounds.right  + pageXOffset,
+					top:    viewportRelativeBounds.top    + pageYOffset,
+					bottom: viewportRelativeBounds.bottom + pageYOffset
+				};
+			} else {
+				if (!target.jquery)
+					target = $(target);
+				targetBox = target.offset();
 			}
-			options = $.extend({}, defaultOptions, options);
+			completeBox(targetBox, target);
 
-			var targetBox = isRange(target) ? getRangeBox(target) : new OffsetBox(target),
-				scrollable = isElement(this) ? new ScrollBox(this) : new WindowBox(this.document),
-				viewportBox = scrollable.parentBox;
+			// Determine viewport box (scrollable dimensions)
+			var viewportBox = options.viewport;
+			if (viewportBox) {
+				completeBox(viewportBox, $(window));
+				// Make viewport relative to document
+				viewportBox.left += pageXOffset;
+				viewportBox.right += pageXOffset;
+				viewportBox.top += pageYOffset;
+				viewportBox.bottom += pageYOffset;
+			} else {
+				// Default to full context object area
+				if ($.isWindow(this)) {
+					viewportBox = {
+						left: window.pageXOffset,
+						top: window.pageYOffset
+					};
+				} else {
+					viewportBox = scrollable.offset();
+				}
+			}
+			completeBox(viewportBox, scrollable);
 
-			// Ensure we have a viewport box
-			if (!viewportBox) {
-				var document = isWindow(this) ? this.document : (this.ownerDocument || this);
-				viewportBox = new Box(
-					0, 0,
-					document.documentElement.clientWidth,
-					document.documentElement.clientHeight,
-					null);
+			// Apply padding
+			if (options.padding) {
+				insetBox(viewportBox, options.padding);
 			}
 
-			// Adjust viewport box
-			if (options.viewport) {
-				viewportBox = adjustViewport(viewportBox, options.viewport);
-			}
-
-			if (options.toPoint) {
-				viewportBox = shrinkViewport(viewportBox, options.toPoint);
-			}
-
-			var targetPoint = getTargetPoint(targetBox, options.point);
-
-			// Get offset
-			var offset = getOffsetFromBox(targetPoint, viewportBox);
-
-			// Scroll element
-			scrollable.scrollTo(
-				-scrollable.origin.left + offset.left,
-				-scrollable.origin.top  + offset.top);
+			// Determine amount to scroll
+			var scrollOffset = computeScrollOffset(targetBox, viewportBox, options);
+			console.log('scrolling', targetBox, 'into', viewportBox, 'by', scrollOffset);
+			scrollable.scrollLeft(scrollable.scrollLeft() + scrollOffset.left);
+			scrollable.scrollTop(scrollable.scrollTop() + scrollOffset.top);
 		});
 	};
 
